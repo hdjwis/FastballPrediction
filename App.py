badd28d00f4c7728c8bb4d23db64e5f5f6f8a613
hdjwis <adarshyraju@gmail.com>
2025-07-10 10:49:16 -0500

    set home to default

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-homePage = st.Page("home.py", title="Home")
+homePage = st.Page("home.py", title="Home", default=True)
 predictPage = st.Page("predict.py", title="Predictions")
 
 pg = st.navigation([homePage, predictPage])
